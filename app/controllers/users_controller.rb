276d2f5a2840d5940db638123d19688a227bfa2a
Harold Seo <harold.seo.612@gmail.com>
2017-09-26 16:15:16 -0700

    new users login automatically and added placeholders

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to cafes_path 
     else 
       redirect_to new_user_path
