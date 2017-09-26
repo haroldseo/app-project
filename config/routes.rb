1bd82acb102afeb81c2235daaad251c4f1e7a8c8
Harold Seo <harold.seo.612@gmail.com>
2017-09-26 11:02:54 -0700

    added bootstrap and created sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'cafes#index'
   resources :users
   resources :cafes
+  resources :sessions, only: [:new, :create]
+  delete '/logout' => 'sessions#destroy', as: :logout
 end
